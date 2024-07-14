e6609c43f8435aa7faef5df6180769264e213b26
Denis Belonogov <52632384+Denis3006@users.noreply.github.com>
2024-07-14 03:32:14 +0200

    add propertyID

--- src/htmlParser.py
+++ src/htmlParser.py
@@ -4,14 +4,13 @@ from bs4 import BeautifulSoup
 import time
 import re
 import datetime
+import URLSearchParams
 
 
-def get_offers(arrival_date, departure_date, n_persons):
-    arrival_date = datetime.datetime.strptime(arrival_date, "%Y-%m-%d").date()
-    departure_date = datetime.datetime.strptime(departure_date, "%Y-%m-%d").date()
-    #n_persons = int(input('Amount  of persons: '))
+def get_offers(request_args):
+    URL = str(URLSearchParams.URLSearchParams(
+        "https://ibe-server.uphotel.agency/ibe-preview/79579851-938d-441c-b6b9-9e24d15aa192#/booking/results?").append(request_args))
 
-    URL = f"https://onhotels.de/#/booking/results?propertyId=CARLTON&arrival={arrival_date.strftime('%Y-%m-%d')}&departure={departure_date.strftime('%Y-%m-%d')}&adults={n_persons}&"
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
@@ -19,27 +18,25 @@ def get_offers(arrival_date, departure_date, n_persons):
     driver = webdriver.Chrome(options=options)
     driver.get(URL)
     time.sleep(1)
-    try:
-        driver.find_element(By.LINK_TEXT, "Alle akzeptieren").click()
-    except:
-        pass
-
-    time.sleep(6)
-    html = re.sub(r'<!.*?->','', driver.page_source)
+    html = re.sub(r'<!.*?->', '', driver.page_source)
     driver.quit()
     soup = BeautifulSoup(html, "html.parser")
-    rooms = soup.body.find('div', attrs={'class':'ibe-room-results-list'})
+    rooms = soup.body.find('div', attrs={'class': 'ibe-room-results-list'})
 
-    offer_text =f'Sehr geehrter Gast, \nwie von Ihnen gewünscht sende ich Ihnen ein Angebot für ein Zimmer für {n_persons} {"Personen" if n_persons > 1 else "Person"} vom {arrival_date.strftime("%d.%m.%Y")} zum {departure_date.strftime("%d.%m.%Y")}.\n\n'
+    offer_text = f'Sehr geehrter Gast, \nwie von Ihnen gewünscht sende ich Ihnen ein Angebot für ein Zimmer für {request_args['adults']} {"Personen" if int(request_args['adults']) > 1 else "Person"} vom {
+        datetime.datetime.strptime(request_args['arrival_date'], "%Y-%m-%d").strftime("%d.%m.%Y")} zum {datetime.datetime.strptime(request_args['departure_date'], "%Y-%m-%d").strftime("%d.%m.%Y")}.\n\n'
 
     for room in rooms.contents:
-        room_prices = room.contents[0].find('div', attrs={'class':'ibe-rate-options'}).contents
+        room_prices = room.contents[0].find(
+            'div', attrs={'class': 'ibe-rate-options'}).contents
 
-        room_name = room.contents[0].find('div', attrs={'class':'ibe-room-title'}).contents[0]
+        room_name = room.contents[0].find(
+            'div', attrs={'class': 'ibe-room-title'}).contents[0]
         for room_price in room_prices:
             if "Nicht erstattbare" not in room_price.contents[0].text:
                 with_breakfast = "mit Frühstück" in room_price.contents[0].text
                 price = room_price.contents[0].text.split('€')[0].strip()
                 city_tax = round(float(price.replace(',', '.')) * 0.075, 2)
-                offer_text += f"{room_name}, {'inkl. Frühstück ' if with_breakfast else ''}zum Preis von {price} € + {str(city_tax).replace('.', ',')} € City-Tax je Zimmer\n"
+                offer_text += f"{room_name}, {'inkl. Frühstück ' if with_breakfast else ''}zum Preis von {
+                    price} € + {str(city_tax).replace('.', ',')} € City-Tax je Zimmer\n"
     return offer_text
--- src/main.py
+++ src/main.py
@@ -10,12 +10,10 @@ app = Flask(__name__)
 CORS(app)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
+
 @app.route('/', methods=['GET'])
 def home():
-   arrival_date = request.args.get('arrival_date')
-   departure_date = request.args.get('departure_date')
-   n_guests = int(request.args.get('n_guests'))
-   return jsonify({'offer': get_offers(arrival_date, departure_date, n_guests)})
+    return jsonify({'offer': get_offers(request.args)})
 
 
 if __name__ == '__main__':
